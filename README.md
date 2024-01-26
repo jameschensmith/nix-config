22ba044c5b1123d1986d319e1e9a8fc590456b1d
James Chen-Smith <james@chen-smith.net>
2024-01-26 14:07:02 -0600

    Add Nix flake

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 My personal [Nix](https://nixos.org) configurations.
 
+## Usage
+
+First, install Nix using the [Determinate Nix
+Installer](https://github.com/DeterminateSystems/nix-installer):
+
+```
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+```
+
+Next, run `nix develop` in the current directory to get into the Nix shell.
+
+### Build and Activate Configuration
+
+```
+home-manager switch --flake .
+```
+
 ## Public Domain
 
 In an effort to dedicate all of my work to the public domain, copyright and
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+    description = "James Chen-Smith's Nix configurations";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, home-manager }:
+    let
+        lib = home-manager.lib;
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs { inherit system; };
+    in
+    {
+        devShells.${system}.default = pkgs.mkShell {
+            packages = [
+                pkgs.home-manager
+            ];
+        };
+
+        homeConfigurations."james.chen-smith" = lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [{
+                home.username = "james.chen-smith";
+                home.homeDirectory = "/Users/james.chen-smith";
+                home.stateVersion = "23.11";
+                home.packages = with pkgs; [
+                    neovim
+                ];
+                programs.home-manager.enable = true;
+            }];
+        };
+    };
+}
