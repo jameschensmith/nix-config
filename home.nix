ce0e009ea4ec3c24c4c5ba8a7dac9375a5c748a6
James Chen-Smith <james@chen-smith.net>
2024-02-02 14:11:40 -0600

    Add git to packages & link git config

--- home.nix
+++ home.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }: {
     home.stateVersion = "23.11";
     home.packages = with pkgs; [
+        git
         just
         kakoune
         neovim
     ];
     programs.home-manager.enable = true;
+    xdg.configFile."git/config".source = ./config/git/config;
 }
