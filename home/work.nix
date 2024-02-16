d8de9a26d7966919706416bcf5b2a460a3b2f5d4
James Chen-Smith <james@chen-smith.net>
2024-02-16 10:29:45 -0600

    Add .NET SDKs to work packages

--- home/work.nix
+++ home/work.nix
@@ -1,5 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
     imports = [ ./shared.nix ];
     home.username = "james.chen-smith";
     home.homeDirectory = "/Users/james.chen-smith";
+    home.packages = with pkgs; [
+        (with dotnetCorePackages; combinePackages [
+            sdk_6_0
+            sdk_8_0
+        ])
+    ];
 }
