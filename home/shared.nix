289a54bedc3ec7f733568d4a6d652762a18f9742
James Chen-Smith <james@chen-smith.net>
2024-07-23 22:11:37 -0500

    Move home config files under home

--- home/shared.nix
+++ home/shared.nix
@@ -26,9 +26,9 @@
         zsh
     ];
     programs.home-manager.enable = true;
-    xdg.configFile."git/config".source = ../config/git/config;
-    xdg.configFile."kak/kakrc".source = ../config/kak/kakrc;
-    xdg.configFile."tmux/tmux.conf".source = ../config/tmux/tmux.conf;
-    home.file.".zshenv".source = ../config/zsh/.zshenv;
-    xdg.configFile."zsh/.zshrc".source = ../config/zsh/.zshrc;
+    xdg.configFile."git/config".source = ./config/git/config;
+    xdg.configFile."kak/kakrc".source = ./config/kak/kakrc;
+    xdg.configFile."tmux/tmux.conf".source = ./config/tmux/tmux.conf;
+    home.file.".zshenv".source = ./config/zsh/.zshenv;
+    xdg.configFile."zsh/.zshrc".source = ./config/zsh/.zshrc;
 }
